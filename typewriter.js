1f87c11be40bd9a2f72b7bdb346d2dbe00e5907f
bricemodeste <bricemodeste@Brices-MacBook-Pro.local>
2019-02-11 23:12:30 +0100

    sounds not set

--- typewriter.js
+++ typewriter.js
@@ -1,8 +1,8 @@
 "use strict";
 let fullText = document.querySelector("#typewriter").textContent;
-const typeSonUn = document.getElementById("typekey1");
-const typeSonDeux = document.getElementById("typekey2");
-const typeSonTrois = document.getElementById("typespace");
+const SonUn = document.getElementById("typekey1");
+const SonDeux = document.getElementById("typekey2");
+const SonTrois = document.getElementById("typespace");
 
 let counter = 0;
 document.querySelector("#typewriter").textContent = "";
