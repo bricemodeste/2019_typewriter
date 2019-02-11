0049bc2a3f8641e831baccd8c224e43add898c0f
Brice Modeste <bricemodeste@Brices-MacBook-Pro.local>
2019-02-11 21:22:57 +0100

    typewriter js working

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,20 @@
 "use strict";
+let fullText = document.querySelector("#typewriter").textContent;
+const typeSoundOne = document.getElementById("typekey1");
+const typeSoundTwo = document.getElementById("typekey2");
+const typeSoundSpace = document.getElementById("typespace");
+
+let counter = 0;
+document.querySelector("#typewriter").textContent = "";
+
+setInterval(() => {
+  if (counter < fullText.length) {
+    document.querySelector("#typewriter").textContent = fullText.substring(
+      0,
+      counter + 1
+    );
+  } else {
+    counter = 0;
+  }
+  counter++;
+}, 100);
